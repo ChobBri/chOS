d169ea6d3e9ec72876284cb7047e615d9adb60a0
Brian Cho <chobbri@outlook.com>
2024-06-30 21:35:50 -0400

    Fix env.sh
    -Stays in the directory where the script was invoked

--- env.sh
+++ env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+CURRENT_DIR="$(pwd)"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR/..
 ARM_GNU_TOOLCHAIN=$(pwd)/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf/bin
 PATH=$PATH:$ARM_GNU_TOOLCHAIN
+
+cd $CURRENT_DIR
